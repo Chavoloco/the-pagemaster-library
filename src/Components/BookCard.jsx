d0f42832c61092db4a333e0437aa4daaec9add27
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-11-24 14:01:20 -0300

    Update BookCard.jsx

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -11,7 +11,7 @@ const BookCard = (book) => {
   return (
     //new yor times api
     <div className="books text-white" onClick={handleClick}>
-      {isSelected ? "Hi" : "Bye"}
+      {isSelected ? "Click" : ""}
       <div className="image">
         <img src={book.book.book_image} alt="books"></img>
       </div>
